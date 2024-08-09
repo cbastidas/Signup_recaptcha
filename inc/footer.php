62be8e9358859266b9f3525fada4d077d6eed06c
Christian Bastidas <cris931201@gmail.com>
2024-08-09 10:00:51 +0200

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,27 @@
+<!-- Footer Start -->
+<footer>
+    
+    
+ <div class="container-fluid main-bottom">
+        <div class="row no-gutters">
+            <div class="col-xl-7 col-lg-9">
+                <div class="row no-gutters partners">
+             
+                    <img src="https://www.neataffiliates.com/wp-content/themes/neataffiliates/images/logos.png" class="img-fluid" alt="">
+                    <!--<img src="assets/images/logos_mobile.png" class="logos-mobile" alt=""> -->
+                </div>
+            </div>
+            <div class="col-xl-5 col-lg-3 main-bottom-links d-flex align-items-end justify-content-end">
+                <div class="menu-footer-menu-container">
+                    <ul id="list-inline" class="list-inline">
+                        <li id="menu-item-164" class="list-inline-item terms menu-item menu-item-type-custom menu-item-object-custom menu-item-164"><a href="https://www.neataffiliates.com/testimonials/" target="_blank">Testimonials</a></li>
+                        <li id="menu-item-185" class="list-inline-item menu-item menu-item-type-post_type menu-item-object-page menu-item-185"><a href="https://www.neataffiliates.com/faq/" target="_blank">FAQ</a></li>
+                        
+                   <!-- <li id="menu-item-164" class="list-inline-item terms menu-item menu-item-type-custom menu-item-object-custom menu-item-164"><a href="#" data-toggle="modal" data-target="#exampleModalLong">Term Of Use</a></li> -->
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>  
+    
+</footer>
---
+++ inc/header.php
@@ -0,0 +1,165 @@
+   
+<header>
+	<div class="container-fluid">
+		<div class="row">
+			<nav class="navbar navbar-expand-lg">
+				<a class="navbar-brand" href="https://www.neataffiliates.com/" target="_blank">&nbsp;</a>
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<button class="mobile-menu">
+					<img src="assets/images/menu_dark.svg" alt="">
+				</button>
+				<div id="navbarSupportedContent" class="collapse navbar-collapse">
+					<ul id="menu-ana-menu" class="navbar-nav ml-auto">
+						<button class="menu-close">
+							<img src="assets/images/close.svg" alt="">
+						</button>
+						<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-161" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-161 nav-item">
+							<a title="Why Join" href="https://www.neataffiliates.com/why-join/" class="nav-link" target="_blank">Why Join</a>
+						</li>
+						<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-183" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-183 nav-item">
+							<a title="Commission Rate" href="https://www.neataffiliates.com/commission-rate/" class="nav-link" target="_blank">Commission Rate</a>
+						</li>
+						<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-186" class="mobile-menu-item menu-item menu-item-type-post_type menu-item-object-page menu-item-186 nav-item">
+							<a title="FAQ" href="https://www.neataffiliates.com/faq/" class="nav-link" target="_blank">FAQ</a>
+						</li>
+						<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-165" class="mobile-menu-item terms menu-item menu-item-type-custom menu-item-object-custom menu-item-165 nav-item">
+							<a title="Term of Use" href="#" class="nav-link" data-toggle="modal" data-target="#exampleModalLong">Term of Use</a>
+						</li>
+						<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-184" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-184 nav-item">
+							<a title="Contact" href="https://www.neataffiliates.com/contact/" class="nav-link" target="_blank">Contact</a>
+						</li>
+						<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-162" class="login-main menu-item menu-item-type-custom menu-item-object-custom menu-item-162 nav-item">
+							<a title="Login" href="#" class="nav-link" data-toggle="modal" data-target="#exampleModal">Login</a>
+						</li>
+						<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="menu-item-163" class="signup-main menu-item menu-item-type-custom menu-item-object-custom menu-item-163 nav-item">
+							<a title="Sign Up" href="#" class="nav-link" data-toggle="modal" data-target="#exampleModal3">Sign Up</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+	</div>
+</header>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Select login platform</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<a href="https://rlmaffpanel.myaffcdn1.com/signin.php" target="_blank" class="select-login-first">
+					<span class="bold" style="font-size: 13px">Bets10, MobilBahis, CasinoMaxi, Casino Metropol</span>
+				</a>
+				<a href="https://thrnaffpanel.thrnaffcdn.com/signin.php" class="select-login-second">
+                    <span class="bold" style="font-size: 13px">Discount Casino, Jetbahis, Rexbet, Betroad, Hovarda, Intobet, GenzoBet, Betelli, DaVegas, BetChip, Milyar, Davegas - India</span>
+                </a>
+				<a href="https://afflogin.vidavegasaff.com/signin.php" class="select-login-third">
+                    <span class="bold" style="font-size: 13px">VIDAVEGAS</span>
+                </a>
+                <a href="https://afflogin.gb1aff.com/signin.php" class="select-login-fourth">
+                    <span class="bold" style="font-size: 13px">BLUFFBET</span>
+                </a>
+			</div>
+			<div class="modal-footer">
+                <span>Please select which platform you want to login</span>
+            </div>
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Login to Real Entertainments Brands</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<div class="form-line">
+					<input type="text" placeholder="Username">
+					</div>
+					<div class="form-line">
+						<input type="password" placeholder="Password" id="password-field" >
+							<span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password">
+								<img src="assets/images/eye.svg" alt="">
+								</span>
+								<a href="#" class="forgot">Forgot password</a>
+							</div>
+							<div class="form-button-line">
+								<button class="btn-big btn-purple ml-auto w-100">Login</button>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<a href="#">Sign up now</a>
+						</div>
+					</div>
+				</div>
+			</div>
+<div class="modal fade" id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Login to Throne Gaming Brands</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+	        </div>
+<div class="modal-body">
+	<div class="form-line">
+		<input type="text" placeholder="Username">
+	</div>
+	<div class="form-line">
+	    <input type="password" placeholder="Password" id="password-field" >
+		<span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password">
+			<img src="assets/images/eye.svg" alt="">
+		</span>
+		<a href="#" class="forgot">Forgot password</a>
+		</div>
+		<div class="form-button-line">
+			<button class="btn-big btn-purple ml-auto w-100">Login</button>
+		</div>
+		</div>
+		<div class="modal-footer">
+			<a href="#">Sign up now</a>
+		</div>
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="exampleModal3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Select signup platform</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+<div class="modal-body">
+                	 <a href="https://rlmaffpanel.myaffcdn1.com/signup.php" target="_blank" class="select-login-first">
+                    <span class="bold" style="font-size: 13px">Bets10, MobilBahis, CasinoMaxi, Casino Metropol</span>
+                </a>
+                <a href="https://signup.neataffiliates.com/" class="select-login-second">
+                    <span class="bold" style="font-size: 13px">Discount Casino, Jetbahis, Rexbet, Betroad, Hovarda, Intobet, GenzoBet, Betelli, DaVegas, BetChip, Milyar, Davegas - India</span>
+                </a>
+				<a href="https://afflogin.vidavegasaff.com/signup.php" class="select-login-third">
+                    <span class="bold" style="font-size: 13px">VIDAVEGAS</span>
+                </a>
+                <a href="https://afflogin.gb1aff.com/signup.php" class="select-login-fourth">
+                    <span class="bold" style="font-size: 13px">BLLUFFBET</span>
+                </a>
+            </div>
+			<div class="modal-footer">
+                <span>Please select which platform you want to login</span>
+            </div>
+		</div>
+	</div>
+</div>  
+    
+    
+    
+    
+    
